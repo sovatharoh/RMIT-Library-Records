8ad7785768466af053b69ca8b6af405e530d2563
s3783867 <s3783867@student.rmit.edu.au>
2022-08-24 18:42:54 +1000

    Test successful and unsuccessful borrow and return

--- src/test/java/au/edu/rmit/ct/LendingManagerImplTest.java
+++ src/test/java/au/edu/rmit/ct/LendingManagerImplTest.java
@@ -1,30 +1,96 @@
 /**
  *
- * Name: John Smith (( Update with your name here ))
- * Student ID: s45045012  (( Update with your ID))
+ * Name: Sovatharo Huy (( Update with your name here ))
+ * Student ID: s3783867  (( Update with your ID))
  *
  * [OPTIONAL: add any notes or comments here about the code]
  */
 
 package au.edu.rmit.ct;
 
+import com.wmw.examples.mockito.library.LendingManagerImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 class LendingManagerImplTest {
-
+    /*
+    TODO
+    Provide at least four differently purposed Junit test cases that cover successful borrow and
+    return transactions (ie 2 x 2 overall)
+     */
     @Test
-    @DisplayName("Example Display Name 1")
-    void borrowBook1() {
-        fail("not implemented yet. Just an example test method.");
+    @DisplayName("Test successful borrow transaction by checking the borrowed book is correct")
+    void checkSuccessfulBorrowBook() {
+        RMITLibraryRecordsDAO lrdao = new RMITLibraryRecordsDAO();
+        LendingManagerImpl lendingManager = new LendingManagerImpl();
+        RMITLibraryItem book = new RMITLibraryItem(100L, "ISBN");
+        lendingManager.setLibraryRecordDAO(lrdao);
+        assertEquals(lendingManager.borrowBook(book).getBook(), book);
     }
 
     @Test
-    @DisplayName("Example Display Name 2")
-    void returnBook1() {
-        fail("not implemented yet. Just an example test method.");
+    @DisplayName("Test successful borrow transaction by checking that the borrowed date is correct")
+    void checkSuccessfulBorrowBook2() {
+        RMITLibraryRecordsDAO lrdao = new RMITLibraryRecordsDAO();
+        LendingManagerImpl lendingManager = new LendingManagerImpl();
+        RMITLibraryItem book = new RMITLibraryItem(100L, "ISBN");
+        lendingManager.setLibraryRecordDAO(lrdao);
+        assertEquals(lendingManager.borrowBook(book).getBorrowingDate(), new Date());
+    }
+    @Test
+    @DisplayName("Test successful return transaction")
+    void checkSuccessfulReturnBook() {
+        RMITLibraryRecordsDAO lrdao = new RMITLibraryRecordsDAO();
+        LendingManagerImpl lendingManager = new LendingManagerImpl();
+        RMITLibraryItem book = new RMITLibraryItem(100L, "ISBN");
+        lendingManager.setLibraryRecordDAO(lrdao);
+        assertEquals(lendingManager.borrowBook(book).getBook(), book);
+        assertEquals(lendingManager.returnBook(book).getBook(), book);
     }
 
+    @Test
+    @DisplayName("Test successful return transaction by checking the returned date is correct")
+    void checkSuccessfulReturnBook2() {
+        RMITLibraryRecordsDAO lrdao = new RMITLibraryRecordsDAO();
+        LendingManagerImpl lendingManager = new LendingManagerImpl();
+        RMITLibraryItem book = new RMITLibraryItem(100L, "ISBN");
+        lendingManager.setLibraryRecordDAO(lrdao);
+        assertEquals(lendingManager.borrowBook(book).getBorrowingDate(), new Date());
+        assertEquals(lendingManager.returnBook(book).getReturningDate(), new Date());
+    }
+    /*
+    TODO
+    Provide at least two differently purposed Junit test cases for unsuccessful borrow / return transactions.
+     */
+    @Test
+    @DisplayName("Test unsuccessful borrow transaction by borrowing a book that hasn't been returned")
+    void checkUnsuccessfulBorrowBook() {
+        RMITLibraryRecordsDAO lrdao = new RMITLibraryRecordsDAO();
+        LendingManagerImpl lendingManager = new LendingManagerImpl();
+        RMITLibraryItem book = new RMITLibraryItem(100L, "ISBN");
+        lendingManager.setLibraryRecordDAO(lrdao);
+        assertEquals(lendingManager.borrowBook(book).getBook(), book);
+        IllegalStateException thrownException = assertThrowsExactly(IllegalStateException.class, () -> {
+            lendingManager.borrowBook(book);
+        });
+        assertEquals(thrownException.getMessage(), "This book is not returned yet.");
+    }
+
+    @Test
+    @DisplayName("Test unsuccessful return transaction by returning a book that hasn't been borrowed")
+    void checkUnsuccesfulReturnBook(){
+        RMITLibraryRecordsDAO lrdao = new RMITLibraryRecordsDAO();
+        LendingManagerImpl lendingManager = new LendingManagerImpl();
+        RMITLibraryItem book = new RMITLibraryItem(100L, "ISBN");
+        lendingManager.setLibraryRecordDAO(lrdao);
+        IllegalStateException thrownException = assertThrowsExactly(IllegalStateException.class, () -> {
+            lendingManager.returnBook(book);
+        });
+        assertEquals(thrownException.getMessage(), "This book is not borrowed.");
+    }
 }
