786252db96abcc38484c63db0bb2682712e99918
s3783867 <s3783867@student.rmit.edu.au>
2022-08-27 16:09:14 +1000

    message

--- src/test/java/au/edu/rmit/ct/LendingManagerImplTestCheckRI.java
+++ src/test/java/au/edu/rmit/ct/LendingManagerImplTestCheckRI.java
@@ -3,7 +3,7 @@
  * Name: Sovatharo Huy
  * Student ID: s3783867  (( Update with your ID))
  *
- * [OPTIONAL: add any notes or comments here about the code]
+ * Only testing failures because successful functionality have been covered in other test cases.
  */
 
 package au.edu.rmit.ct;
